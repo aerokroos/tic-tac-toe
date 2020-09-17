623ea85394c3e1d070f238cd172ffeccb745b65a
jalbertoornelass <albertoornelass44@gmail.com>
2020-09-16 20:18:32 -0700

    fixing ttt

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,12 @@
-const board = ["-", "-", "-",
-            "-", "-", "-", 
-            "-", "-", "-"];
+const board = ["-","-","-",
+            "-","-","-",
+            "-","-","-"];
 
 let player = "-";
 let computer = "";
-let positionComputer;
-
 let o = document.getElementById('O');
 let x =  document.getElementById('X');
-
 let option;
-
-
 
 function chooseX() {
     playerValue = x
@@ -23,66 +18,81 @@ function chooseO() {
     player = playerValue.id;
 }
 
-function putChoose(idItem, position) {
+function putChoose(playerPosition) {
     if (player === "X") {
-        option = document.getElementById(idItem.id);
+        console.log(playerPosition);
+        option = document.getElementById(playerPosition);
         option.src = "img/x.png";
-        board[position] = "X";
-        
+        board[playerPosition] = "X";
+        console.log(board); 
         computer ="O";
-        console.log(computer);
-        computerIsPlaying(computer, idItem);
+        computerIsPlaying(computer);
+    
     } else if (player === "O") {
-        option = document.getElementById(idItem.id);
+        option = document.getElementById(playerPosition);
         option.src = "img/circle.jpg";
-        board[position] = "O";
-
+        board[playerPosition] = "O";
+        console.log(board);
         computer = "X";
-        console.log(computer);
-        computerIsPlaying(computer, idItem);
+        computerIsPlaying(computer);
     }
-    winner(board);
 }
 
 function randomPosition(){
-    positionComputer = Math.floor((Math.random() * 8) + 1);
-    return positionComputer;
+    computerPosition = Math.floor((Math.random() * 8) + 1);
+    return computerPosition;
 }
 
-function computerIsPlaying(computer, idItem) {
+function computerIsPlaying(computer) {
+    computerPosition = randomPosition();
     if (computer === "X") {
-        console.log("PC X");
-        let option = document.getElementById(idItem.id);
-        option.src = "img/x.png";
-    } else if(computer === "O") {
-        console.log("PC O");
-        let option = document.getElementById(idItem.id);
-        option.src = "img/circle.jpg";
+        if (board[computerPosition] === "-") {
+            let option = document.getElementById(computerPosition);
+            option.src = "img/x.png";
+            board[computerPosition] = "X";
+            checkWinner();
+        } else {
+            computerIsPlaying(computer);
+        }
+    } else if (computer === "O") {
+        if (board[computerPosition] === "-") {
+            let option = document.getElementById(computerPosition);
+            option.src = "img/circle.jpg";
+            board[computerPosition] = "O";
+            checkWinner();
+        } else {
+            computerIsPlaying(computer);
+        }
     }
 }
 
-
-
-function winner(board){
-    if (board[0] == "X" && board[1] == "X" && board[2] == "X"){
-        console.log("You WON!!!");
-    } else if (board[3] == "X" && board[4] == "X" && board[5] == "X"){
-        console.log("You WON!!!");
-    } else if (board[6] == "X" && board[7] == "X" && board[8] == "X"){
-        console.log("You WON!!!");
-    } else if (board[2] == "X" && board[4] == "X" && board[6] == "X") {
-        console.log("You WON!!!");
-    } else if (board[0] == "X" && board[4] == "X" && board[8] == "X") {
-        console.log("You WON!!!");
+function checkWinner(){
+    if (player === "X") {
+        if (board[0] == "X" && board[1] == "X" && board[2] == "X"){
+            console.log("You WON!!!");
+        } else if (board[3] == "X" && board[4] == "X" && board[5] == "X"){
+            console.log("You WON!!!");
+        } else if (board[6] == "X" && board[7] == "X" && board[8] == "X"){
+            console.log("You WON!!!");
+        } else if (board[2] == "X" && board[4] == "X" && board[6] == "X") {
+            console.log("You WON!!!");
+        } else if (board[0] == "X" && board[4] == "X" && board[8] == "X") {
+            console.log("You WON!!!");
+        }
+    } else if (player === "O") {
+        if (board[0] == "O" && board[1] == "O" && board[2] == "O"){
+            console.log("You WON!!!");
+        } else if (board[3] == "O" && board[4] == "O" && board[5] == "X"){
+            console.log("You WON!!!");
+        } else if (board[6] == "O" && board[7] == "O" && board[8] == "O"){
+            console.log("You WON!!!");
+        } else if (board[2] == "O" && board[4] == "O" && board[6] == "O") {
+            console.log("You WON!!!");
+        } else if (board[0] == "O" && board[4] == "X" && board[8] == "O") {
+            console.log("You WON!!!");
+        }
     }
 }
-
-
-
-
-
-
-// showBoard(board);
 
 
 
